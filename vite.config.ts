bafe83157f4bf0e51084ffa1fad31fe79100ffd6
Franz <franz.recinos@gmail.com>
2024-04-12 13:07:34 -0400

    :white_check_mark: (test) vitest
    added vitest and a small test example

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
-import { vitePlugin as remix } from '@remix-run/dev'
-import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
+import { installGlobals } from '@remix-run/node'
+import react from '@vitejs/plugin-react'
+import { vitePlugin as remix } from '@remix-run/dev'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 installGlobals()
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    process.env.VITEST
+      ? react()
+      : remix({
+          appDirectory: 'app',
+          ignoredRouteFiles: ['**/.*'],
+          future: {},
+          serverModuleFormat: 'esm',
+        }),
+  ],
+  test: {
+    clearMocks: true,
+    environment: 'happy-dom',
+    globals: false,
+    passWithNoTests: true,
+    setupFiles: ['./tests/setup-test-env.ts'],
+    watch: false,
+  },
 })
